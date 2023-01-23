57b7bec2bb7540b288dbbd91cbc69e37c5b96256
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2023-01-23 20:26:41 +0100

    Add functions to translate strings

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,8 +1,10 @@
+""" IBM Watson Translator Instance"""
+
 import os
-import json
 from dotenv import load_dotenv
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
 
 load_dotenv()
 
@@ -16,3 +18,27 @@ language_translator = LanguageTranslatorV3(
 )
 
 language_translator.set_service_url(url)
+
+
+def english_to_french(english_text):
+    """ Function to translate english text
+        to french text """
+    french_text = language_translator\
+        .translate(
+            text=english_text,
+            model_id='en-fr'
+        ).get_result()
+
+    return french_text["translations"][0]["translation"]  # type: ignore
+
+
+def french_to_english(french_text):
+    """ Function to translate english text
+        to french text """
+    french_text = language_translator\
+        .translate(
+            text=french_text,
+            model_id='fr-en'
+        ).get_result()
+
+    return french_text["translations"][0]["translation"]  # type: ignore
