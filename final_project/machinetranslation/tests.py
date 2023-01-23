b25812901471dcc63be85003e67663c13509c6b9
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2023-01-23 20:48:03 +0100

    Separate tests for each from language

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -5,7 +5,7 @@ import unittest
 from translator import english_to_french, french_to_english
 
 
-class TestTranlator(unittest.TestCase):
+class TestEnglishToFrench(unittest.TestCase):
     """ Class to test text from english to french """
 
     def test_null_english_to_french(self):
@@ -13,19 +13,23 @@ class TestTranlator(unittest.TestCase):
         with self.assertRaises(ValueError):
             english_to_french(None)
 
-    def test_null_french_to_english(self):
-        """ Function to test against input of null """
-        with self.assertRaises(ValueError):
-            french_to_english(None)
-
     def test_word_hello_to_bonjour(self):
         """ Function to test translation from
             english to french """
         self.assertEqual(english_to_french("Hello"), "Bonjour")
 
+
+class TestFrenchToEnglish(unittest.TestCase):
+    """ Class to test text from french to english """
+
+    def test_null_french_to_english(self):
+        """ Function to test against input of null """
+        with self.assertRaises(ValueError):
+            french_to_english(None)
+
     def test_word_bonjour_to_hello(self):
         """ Function to test translation from
-            english to french """
+            french to english """
         self.assertEqual(french_to_english("Bonjour"), "Hello")
 
 
