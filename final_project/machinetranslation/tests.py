6e799604842032761e45bd350f14dad81f05052c
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2023-01-23 20:27:37 +0100

    Add test cases

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,32 @@
+""" Tests for translator.py """
+
+import unittest
+
+from translator import english_to_french, french_to_english
+
+
+class TestEnglishToFrench(unittest.TestCase):
+    """ Class to test text from english to french """
+
+    def test_null_english_to_french(self):
+        """ Function to test against input of null """
+        with self.assertRaises(ValueError):
+            english_to_french(None)
+
+    def test_null_french_to_english(self):
+        """ Function to test against input of null """
+        with self.assertRaises(ValueError):
+            french_to_english(None)
+
+    def test_word_hello_to_bonjour(self):
+        """ Function to test translation from
+            english to french """
+        self.assertEqual(english_to_french("Hello"), "Bonjour")
+
+    def test_word_bonjour_to_hello(self):
+        """ Function to test translation from
+            english to french """
+        self.assertEqual(french_to_english("Bonjour"), "Hello")
+
+
+unittest.main()
