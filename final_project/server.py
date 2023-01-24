453682ea15bd14dec9c2fcb242d95fb15f207437
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2023-01-24 12:28:48 +0100

    Add render index and return translated text

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,35 @@
-from machinetranslation import translator
+""" Translation server and endpoints """
 from flask import Flask, render_template, request
-import json
+from machinetranslation.translator import english_to_french, french_to_english
 
-app = Flask("Web Translator")
+app = Flask("Web Translator",
+            static_folder="/Users/mturous/Dev/xzceb-flask_eng_fr/final_project/static",
+            template_folder="/Users/mturous/Dev/xzceb-flask_eng_fr/final_project/templates")
+
 
 @app.route("/englishToFrench")
-def englishToFrench():
+def englishToErench():
+    """ Translate english to french """
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    print(textToTranslate)
+
+    return english_to_french(textToTranslate)
+
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
+    """ Translate french to english """
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    return french_to_english(textToTranslate)
+
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    """ Render index page """
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
